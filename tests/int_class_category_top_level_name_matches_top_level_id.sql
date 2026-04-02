2c1afc476d1653f9567d0c821889a498b4f07643
Rosie Taylor <rosietaylor@monzo.com>
2026-04-02 13:49:06 +0100

    deleting tests not needed

--- tests/int_class_category_top_level_name_matches_top_level_id.sql
+++
@@ -1,6 +0,0 @@
-select child.*
-from {{ ref('int_class_category_hierarchy') }} child
-left join {{ ref('stg_class_categories') }} top_level
-    on child.top_level_category_id = top_level.category_id
-where child.top_level_category_id is not null
-  and child.top_level_category_name != top_level.category_name
